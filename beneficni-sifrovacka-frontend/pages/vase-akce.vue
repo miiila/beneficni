0ded3efddf425a0487f011aae763752af124c089
miiila <me@milavotradovec.cz>
2023-10-18 23:12:07 +0200

    team actions

--- beneficni-sifrovacka-frontend/pages/vase-akce.vue
+++ beneficni-sifrovacka-frontend/pages/vase-akce.vue
@@ -13,36 +13,35 @@
         <th>Šifra</th>
         <th>Heslo</th>
       </tr>
-      </thead>
-      <tbody>
-    <template v-for="action in actions" :key="action.teamName">
-      <tr v-if="action.action == 'solved'" class="success">
-        <td>{{ formatTimestamp(action.timestamp) }}</td>
-        <td>Vyřešeno</td>
-        <td>{{action.puzzleId}}</td>
-        <td>{{action.payload}}</td>
-      </tr>
-      <tr v-else-if="action.action == 'unlocked'">
-        <td>{{ formatTimestamp(action.timestamp) }}</td>
-        <td>Odemčeno</td>
-        <td>{{action.puzzleId}}</td>
-        <td></td>
-      </tr>
-      <tr v-else-if="action.action == 'failed'" class="failed">
-        <td>{{ formatTimestamp(action.timestamp) }}</td>
-        <td>Špatný pokus</td>
-        <td>{{action.puzzleId}}</td>
-        <td>{{action.payload}}</td>
-      </tr>
-    </template>
-    </tbody> 
+    </thead>
+    <tbody>
+      <template v-for="action in actions" :key="action.teamName">
+        <tr v-if="action.action == 'solved'" class="success">
+          <td>{{ formatTimestamp(action.timestamp) }}</td>
+          <td>Vyřešeno</td>
+          <td>{{ action.puzzleId }}</td>
+          <td>{{ action.payload }}</td>
+        </tr>
+        <tr v-else-if="action.action == 'unlocked'">
+          <td>{{ formatTimestamp(action.timestamp) }}</td>
+          <td>Odemčeno</td>
+          <td>{{ action.puzzleId }}</td>
+          <td />
+        </tr>
+        <tr v-else-if="action.action == 'failed'" class="failed">
+          <td>{{ formatTimestamp(action.timestamp) }}</td>
+          <td>Špatný pokus</td>
+          <td>{{ action.puzzleId }}</td>
+          <td>{{ action.payload }}</td>
+        </tr>
+      </template>
+    </tbody>
   </table>
 </template>
 <script setup lang="ts">
 import { useAuthStore, type AuthStore } from '~/stores/auth'
 const authStore: AuthStore = useAuthStore()
 const { data: actions } = await useFetch<any[]>('/api/team-actions', { headers: { Authorization: `Bearer ${authStore.jwt}` } })
-
 
 function formatTimestamp (d: string) {
   const date = new Date(d)
--- beneficni-sifrovacka-frontend/server/api/team-actions.get.ts
+++ beneficni-sifrovacka-frontend/server/api/team-actions.get.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
   const config: { public: { gameFinished: boolean, registrationFinished: boolean, gameStarted: boolean } } = useRuntimeConfig()
   if (!config.public.gameStarted) {
     return []
-  } 
+  }
   const query = qs.stringify({ fields: ['id'], populate: { team_actions: { fields: '*', populate: { puzzle: { fields: ['id'] } } } }, sort: 'id' }, {
     encodeValuesOnly: true // prettify URL
   })
@@ -14,15 +14,15 @@ export default defineEventHandler(async (event) => {
   try {
     const teamActionsRaw = await $fetch(teamActionsUrl, { method: 'GET', headers: { Authorization: headers.authorization! } })
 
-  return teamActionsRaw.team_actions.map((teamAction: any) => {
-    return {
-      id: teamAction.id,
-      puzzleId: teamAction.puzzle.id,
-      action: teamAction.action,
-      timestamp: teamAction.timestamp,
-      payload: teamAction.payload ?? "HESLO"
-    }
-  })
+    return teamActionsRaw.team_actions.map((teamAction: any) => {
+      return {
+        id: teamAction.id,
+        puzzleId: teamAction.puzzle.id,
+        action: teamAction.action,
+        timestamp: teamAction.timestamp,
+        payload: teamAction.payload ?? 'HESLO'
+      }
+    })
   } catch (err: any) {
     console.error(err)
     throw createError({ statusCode: err.data?.error.status, statusMessage: 'Something bad happened when fetching the team' })
